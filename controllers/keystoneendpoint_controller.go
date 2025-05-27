c641acf5f95471c2c09a7db3eb755347c570674c
Martin Schuppert <mschuppert@redhat.com>
2025-05-27 12:55:52 +0200

    Add GetKeystoneEndpointUrlsForServices and GetHashforKeystoneEndpointUrlsForServices
    GetKeystoneEndpointUrlsForServices returns all URLs currently
    registered. Visibility can be admin, public or internal. If it
    is nil, all URLs are returned. Optional services parameter can be
    provided to only return the values for those services matching the
    `service` (common.AppSelector) label key.
    
    GetHashforKeystoneEndpointUrlsForServices returns a hash for a list
    of endpoint URLs currently registered.
    Visibility parameter to filter services like the above.
    
    Also adds ValidateGeneration method for an KeystoneEndpoint to
    check if the ObservedGeneration equals the Generation.
    
    Jira: OSPRH-16994
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- controllers/keystoneendpoint_controller.go
+++ controllers/keystoneendpoint_controller.go
@@ -34,6 +34,7 @@ import (
 	helper "github.com/openstack-k8s-operators/lib-common/modules/common/helper"
 	util "github.com/openstack-k8s-operators/lib-common/modules/common/util"
 	openstack "github.com/openstack-k8s-operators/lib-common/modules/openstack"
+	"golang.org/x/exp/slices"
 	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 )
 
@@ -130,6 +131,15 @@ func (r *KeystoneEndpointReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		// Register overall status immediately to have an early feedback e.g. in the cli
 		return ctrl.Result{}, nil
 	}
+
+	if instance.Status.EndpointIDs == nil {
+		instance.Status.EndpointIDs = map[string]string{}
+	}
+	if instance.Status.Endpoints == nil {
+		instance.Status.Endpoints = []keystonev1.Endpoint{}
+	}
+
+	instance.Status.ObservedGeneration = instance.Generation
 
 	// If we're not deleting this and the service object doesn't have our finalizer, add it.
 	if instance.DeletionTimestamp.IsZero() && controllerutil.AddFinalizer(instance, helper.GetFinalizer()) {
@@ -538,11 +548,25 @@ func (r *KeystoneEndpointReconciler) reconcileEndpoints(
 				instance, err)
 		}
 
-		if instance.Status.EndpointIDs == nil {
-			instance.Status.EndpointIDs = map[string]string{}
-		}
 		if _, ok := instance.Spec.Endpoints[endpointType]; ok && endpointID != "" {
 			instance.Status.EndpointIDs[endpointType] = endpointID
+		}
+
+		// validate if endpoint is already in the endpoint status list
+		f := func(e keystonev1.Endpoint) bool {
+			return e.Interface == endpointType
+		}
+		idx := slices.IndexFunc(instance.Status.Endpoints, f)
+		if idx >= 0 {
+			instance.Status.Endpoints[idx].ID = endpointID
+			instance.Status.Endpoints[idx].URL = endpointURL
+		} else {
+			instance.Status.Endpoints = append(instance.Status.Endpoints,
+				keystonev1.Endpoint{
+					Interface: endpointType,
+					URL:       endpointURL,
+					ID:        endpointID,
+				})
 		}
 	}
 
